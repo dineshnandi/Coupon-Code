450a9f65a835eb741916021100a591e1f0b7c43b
dineshnandi <dineshnandi082@gmail.com>
2018-12-04 14:16:14 +0530

    Add files via upload

---
+++ CouponCode.java
@@ -0,0 +1,25 @@
+import java.security.SecureRandom;
+import java.util.Random;
+
+
+public class CouponCode {
+
+	public String createRandomCode(int codeLength){   
+	     char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890".toCharArray();
+	        StringBuilder sb = new StringBuilder();
+	        Random random = new SecureRandom();
+	        for (int i = 0; i < codeLength; i++) {
+	            char c = chars[random.nextInt(chars.length)];
+	            sb.append(c);
+	        }
+	        String output = sb.toString();
+	        System.out.println(output);
+	        return output ;
+	    }
+	
+	public static void main(String args[]){
+		int code = 8;
+		CouponCode c = new CouponCode();
+		c.createRandomCode(code);
+	}
+}
